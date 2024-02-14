b1743bc6a9a29bdebe7571e19601375aea1f4006
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2024-02-14 20:52:36 +0530

    nav-bar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Pages/Video/Video.jsx
@@ -0,0 +1,7 @@
+import "./Video.css";
+
+const Video = () => {
+  return <div></div>;
+};
+
+export default Video;
