8ba91d55cce0e3041894080b4f36a26f943971e0
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2024-02-19 19:28:42 +0530

    feed cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Home.css";
+import Feed from "../../Components/Feed/Feed";
 
 const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`contaioner ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
