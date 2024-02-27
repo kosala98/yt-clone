c5d4bce1be38454c38bf343092d266afbd6d4dec
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2024-02-27 19:50:18 +0530

    video recom:

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,15 @@
+import "./Recommended.css";
+import thumbnail1 from "../../assets/thumbnail1.png";
+import thumbnail2 from "../../assets/thumbnail2.png";
+import thumbnail3 from "../../assets/thumbnail3.png";
+import thumbnail4 from "../../assets/thumbnail4.png";
+import thumbnail5 from "../../assets/thumbnail5.png";
+import thumbnail6 from "../../assets/thumbnail6.png";
+import thumbnail7 from "../../assets/thumbnail7.png";
+import thumbnail8 from "../../assets/thumbnail8.png";
+
+const Recommended = () => {
+  return <div></div>;
+};
+
+export default Recommended;
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
+import Recommended from "../../Components/Recommended/Recommended";
 import "./Video.css";
 
 const Video = () => {
   return (
     <div className="play-container">
       <PlayVideo />
+      <Recommended />
     </div>
   );
 };
