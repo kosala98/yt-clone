141343703f326532cddd2ef131c92fe357092166
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2024-02-29 22:41:23 +0530

    recom-vid-com

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -9,7 +9,74 @@ import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
 
 const Recommended = () => {
-  return <div></div>;
+  return (
+    <div className="recommended">
+      <div className="side-video-list">
+        <img src={thumbnail1} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail2} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail3} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail4} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail5} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail6} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail7} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+      <div className="side-video-list">
+        <img src={thumbnail8} alt="" />
+        <div className="vid-info">
+          <h4>Best travel video for you in 2024</h4>
+          <p>Great Travel</p>
+          <p>199K Views</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Recommended;
