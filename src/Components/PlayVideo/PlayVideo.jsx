90535286517872f6190e13697b0572f04c9a509a
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2024-03-23 21:05:03 +0530

    remove img

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,11 +1,10 @@
 import "./PlayVideo.css";
-import video1 from "../../assets/video.mp4";
+
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
-import jack from "../../assets/jack.png";
-import user_profile from "../../assets/user_profile.jpg";
+
 import { useEffect, useState } from "react";
 import { API_KEY, value_converter } from "../../data";
 import moment from "moment";
